05de049e86d6edd778cef47566825cbdc29c4bfe
Hexanima <nicolasfilippelli22@gmail.com>
2022-12-07 14:38:58 -0300

    agrego las html de cada uno

--- app.js
+++ app.js
@@ -10,6 +10,27 @@ res.sendFile(path.join(__dirname, "/views/index.html"))
 
 app.get('/babbage', function(req, res) {
     res.sendFile(path.join(__dirname, "/views/babbage.html"));
+
+app.get('/berners-lee', function(req, res) {
+    res.sendFile(path.join(__dirname, "/views/berners-lee.html"));
+    }
+app.get('/clarke', function(req, res) {
+    res.sendFile(path.join(__dirname, "/views/clarke.html"));
+
+app.get('/hamilton', function(req, res) {
+    res.sendFile(path.join(__dirname, "/views/hamilton.html"));
+
+app.get('/hopper', function(req, res) {
+    res.sendFile(path.join(__dirname, "/views/hopper.html"));
+
+app.get('/lovelace', function(req, res) {
+    res.sendFile(path.join(__dirname, "/views/lovelace.html"));
+
+app.get('/turing', function(req, res) {
+    res.sendFile(path.join(__dirname, "/views/turing.html"));
+
+app.get('/Holand', function(req, res) {
+    res.sendFile(path.join(__dirname, "/views/Holand.html"));
 })
 
 app.listen("3030", ()=>{console.log("el server ta listo")});
