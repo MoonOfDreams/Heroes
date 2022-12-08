fa17abc64c8314f1753cdb6bdf51d8f928283d02
Hexanima <nicolasfilippelli22@gmail.com>
2022-12-07 23:50:28 -0300

    agrego algunas Heroes del mundo de la programacion

--- app.js
+++ app.js
@@ -9,28 +9,29 @@ res.sendFile(path.join(__dirname, "/views/index.html"))
 })
 
 app.get('/babbage', function(req, res) {
-    res.sendFile(path.join(__dirname, "/views/babbage.html"));
+    res.sendFile(path.join(__dirname, "/views/babbage.html"))});
 
 app.get('/berners-lee', function(req, res) {
     res.sendFile(path.join(__dirname, "/views/berners-lee.html"));
-    }
+    })
 app.get('/clarke', function(req, res) {
-    res.sendFile(path.join(__dirname, "/views/clarke.html"));
+    res.sendFile(path.join(__dirname, "/views/clarke.html"))});
 
 app.get('/hamilton', function(req, res) {
-    res.sendFile(path.join(__dirname, "/views/hamilton.html"));
+    res.sendFile(path.join(__dirname, "/views/hamilton.html"))});
 
 app.get('/hopper', function(req, res) {
-    res.sendFile(path.join(__dirname, "/views/hopper.html"));
+    res.sendFile(path.join(__dirname, "/views/hopper.html"))});
 
 app.get('/lovelace', function(req, res) {
-    res.sendFile(path.join(__dirname, "/views/lovelace.html"));
+    res.sendFile(path.join(__dirname, "/views/lovelace.html"))});
 
 app.get('/turing', function(req, res) {
-    res.sendFile(path.join(__dirname, "/views/turing.html"));
+    res.sendFile(path.join(__dirname, "/views/turing.html"))});
 
 app.get('/Holand', function(req, res) {
     res.sendFile(path.join(__dirname, "/views/Holand.html"));
 })
 
-app.listen("3030", ()=>{console.log("el server ta listo")});
+app.listen("3030", ()=>{console.log("el server ta listo")})
+
